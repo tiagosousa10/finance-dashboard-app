2b954dbab2b77117a0b5e1f08c6ef1cfb7d71bf5
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-31 19:18:06 +0100

    mongoose configuration

--- server/index.js
+++ server/index.js
@@ -21,3 +21,13 @@ app.use(cors()); // to enable cors
 console.log("hello")
 
 /*MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));  
+  })
+  .catch((error) => console.log(`${error} did not connect`));
