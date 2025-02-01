6c972d4d87335e1c12a74fa0af752972633fe909
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-01 20:59:28 +0100

    creating the first chart with recharts

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,14 +1,50 @@
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
+import { useMemo } from 'react'
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 type Props = {}
 
 const Row1 = (props: Props) => {
   const {data} = useGetKpisQuery()
+  console.log("🚀 ~ Row1 ~ data:", data)
+  
+  const revenueExpenses = useMemo(()=> {
+    return (
+      data &&
+      data[0].monthlyData.map(({month, revenue, expenses}) => {
+        return {
+          name: month.substring(0, 3), // substring to get first 3 letters of month
+          revenue: revenue,
+          expenses: expenses
+        }
+      })
+    )
+  }, [data])
 
   return (
    <>
-   <DashboardBox gridArea="a"></DashboardBox>
+   <DashboardBox gridArea="a">
+    <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip  />
+            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+   </DashboardBox>
    <DashboardBox gridArea="b"></DashboardBox>
    <DashboardBox gridArea="c"></DashboardBox>
    </>
--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 /*CONFIGURATIONS */
 dotenv.config();
@@ -30,5 +32,9 @@ mongoose
   })
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));  
+
+    // ADD DATA ONE TIME OR AS NEEDED
+    // await mongoose.connection.db.dropDatabase(); // before inserting data drop database
+    // KPI.insertMany(kpis); // insert data into database
   })
   .catch((error) => console.log(`${error} did not connect`));
