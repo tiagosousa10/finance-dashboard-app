aa546b782571ba8aabaec0255442e4a6e32207bb
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-03 17:43:40 +0100

    transactions route and model created

--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-import Product from './models/Product.js';
-import KPI from "./models/KPI.js";
-
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
 
-import { kpis, products } from "./data/data.js";
+import Product from './models/Product.js';
+import KPI from "./models/KPI.js";
+import Transaction from "./models/Transaction.js"
+
+import { kpis, products, transactions } from "./data/data.js";
 
 /*CONFIGURATIONS */
 dotenv.config();
@@ -29,6 +31,7 @@ app.use(cors()); // to enable cors
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes)
+app.use("/transaction", transactionRoutes);
 
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -43,6 +46,7 @@ mongoose
     // ADD DATA ONE TIME OR AS NEEDED
     // await mongoose.connection.db.dropDatabase(); // before inserting data drop database
     // KPI.insertMany(kpis); // insert data into database
-     Product.insertMany(products);
+     //Product.insertMany(products);
+     //Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
