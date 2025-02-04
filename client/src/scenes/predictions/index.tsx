cf19d7090bf88528ab421ef48fd0094e58b4712a
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-04 22:05:25 +0100

    fixing some issues

--- client/src/scenes/predictions/index.tsx
+++ client/src/scenes/predictions/index.tsx
@@ -34,84 +34,80 @@ const Predictions = () => {
       })
    }, [kpiData])
 
-  return (
-    <DashboardBox 
-      width={"100%"}
-      height={"100%"}
-      padding={"1rem"}
-      overflow={"hidden"}
-    > 
-      <FlexBetween m="1rem 2.5rem" gap={"1rem"}>
-         <Box>
-            <Typography variant='h3' >
-               Revenue and Predictions
+   return (
+      <DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
+        <FlexBetween m="1rem 2.5rem" gap="1rem">
+          <Box>
+            <Typography variant="h3">Revenue and Predictions</Typography>
+            <Typography variant="h6">
+              charted revenue and predicted revenue based on a simple linear
+              regression model
             </Typography>
-            <Typography variant='h6'>
-               charted revenue and predicted revenue based on a simple linear regression model
-            </Typography>
-         </Box>
-         <Button 
+          </Box>
+          <Button
             onClick={() => setIsPredictions(!isPredictions)}
             sx={{
-               color: palette.grey[900],
-               backgroundColor: palette.grey[700],
-               boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,0.4)",
+              color: palette.grey[900],
+              backgroundColor: palette.grey[700],
+              boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
             }}
-         >
+          >
             Show Predicted Revenue for Next Year
-         </Button>
-      </FlexBetween>
-
-       <ResponsiveContainer width="100%" height="100%">
-         <LineChart              
+          </Button>
+        </FlexBetween>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
             data={formattedData}
             margin={{
-               top: 20,
-               right: 75,
-               left: 20,
-               bottom: 80,
+              top: 20,
+              right: 75,
+              left: 20,
+              bottom: 80,
             }}
-         >
-            <CartesianGrid strokeDasharray="3 3"  stroke={palette.grey[800]} />
-            <XAxis dataKey="name" tickLine={false} style={{fontSize: "10px"}} > 
-               <Label value ="Month" offset={-5} position="insideBottom" />
+          >
+            <CartesianGrid strokeDasharray="3 3" stroke={palette.grey[800]} />
+            <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }}>
+              <Label value="Month" offset={-5} position="insideBottom" />
             </XAxis>
-            <YAxis  
-               domain={[12000, 26000]}
-               tickLine={false} 
-               axisLine={{strokeWidth : '0'}} 
-               style={{fontSize: "10px"}}
-               tickFormatter={(value) => `$${value}`}
-            > 
-               <Label value="Revenue in USD" offset={-5} position="insideLeft" angle={-90} />
+            <YAxis
+              domain={[12000, 26000]}
+              axisLine={{ strokeWidth: "0" }}
+              style={{ fontSize: "10px" }}
+              tickFormatter={(v) => `$${v}`}
+            >
+              <Label
+                value="Revenue in USD"
+                angle={-90}
+                offset={-5}
+                position="insideLeft"
+              />
             </YAxis>
-            <Tooltip  />
-            <Legend verticalAlign='top' />
-            <Line 
-               type="monotone"
-               dataKey="Actual Revenue"
-               stroke={palette.primary.main}
-               strokeDasharray={0}
-               dot={{strokeWidth:5}}
+            <Tooltip />
+            <Legend verticalAlign="top" />
+            <Line
+              type="monotone"
+              dataKey="Actual Revenue"
+              stroke={palette.primary.main}
+              strokeWidth={0}
+              dot={{ strokeWidth: 5 }}
             />
-            <Line 
-               type="monotone"
-               dataKey="Regression Line"
-               stroke="#8884d8"
-               dot={false}
+            <Line
+              type="monotone"
+              dataKey="Regression Line"
+              stroke="#8884d8"
+              dot={false}
             />
             {isPredictions && (
-               <Line 
-                  type="monotone"
-                  dataKey="Predicted Revenue"
-                  stroke={palette.secondary[500]}
-               />
+              <Line
+                strokeDasharray="5 5"
+                dataKey="Predicted Revenue"
+                stroke={palette.secondary[500]}
+              />
             )}
-         </LineChart>
-       </ResponsiveContainer>
-    </DashboardBox>
-  )
-}
-
+          </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+    );
+  };
 
 export default Predictions;
