75a98c072376c2829b71d44465d366a6bd30add7
tiagosousa <tiagosousa.tams@hotmail.com>
2025-02-04 22:02:11 +0100

    building machine learning prediction

--- client/src/scenes/predictions/index.tsx
+++ client/src/scenes/predictions/index.tsx
@@ -1,17 +1,38 @@
 import DashboardBox from '@/components/DashboardBox'
 import FlexBetween from '@/components/FlexBetween'
-import { useGetKpisQuery } from '@/state/api'
+import { useMemo, useState } from 'react'
 import { Box, Button, Typography, useTheme } from '@mui/material'
-import { useState } from 'react'
+import { useGetKpisQuery } from '@/state/api'
 import { CartesianGrid, Label, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import regression, { DataPoint } from "regression";
 
-type Props = {}
 
-const Predictions = (props: Props) => {
+const Predictions = () => {
    const {palette} = useTheme()
    const [isPredictions,setIsPredictions] = useState(false) 
    const {data: kpiData} = useGetKpisQuery()
 
+
+   const formattedData = useMemo(() => {
+      if(!kpiData) return [] // if kpiData is undefined, return an empty array
+      const monthData = kpiData[0].monthlyData; // get monthlyData from kpiData
+
+      const formatted : Array<DataPoint> = monthData.map( // DataPoint is an array of [index, revenue] -> [0, 100], [1, 200], [2, 300]
+         ({revenue}, i: number) => { // get revenue and index
+         return [i, revenue] // return an array of [index, revenue]
+      })
+
+      const regressionLine = regression.linear(formatted) // get regression line from formatted data
+
+      return monthData.map(({month, revenue}, i: number) => {
+         return { // return an array of objects
+            name: month,
+            "Actual Revenue": revenue,
+            "Regression Line" : regressionLine.points[i][1],  //means [0, 100], [1, 200], [2, 300]
+            "Predicted Revenue" : regressionLine.predict(i + 12)[1], // predict revenue for next year
+         }
+      })
+   }, [kpiData])
 
   return (
     <DashboardBox 
@@ -79,6 +100,13 @@ const Predictions = (props: Props) => {
                stroke="#8884d8"
                dot={false}
             />
+            {isPredictions && (
+               <Line 
+                  type="monotone"
+                  dataKey="Predicted Revenue"
+                  stroke={palette.secondary[500]}
+               />
+            )}
          </LineChart>
        </ResponsiveContainer>
     </DashboardBox>
